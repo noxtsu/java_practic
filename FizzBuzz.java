b30ac1ad12ce43c6ab26d8be74847a50d5a4f788
noxtsu <94978861+noxtsu@users.noreply.github.com>
2022-07-12 02:44:48 +0900

    fizzbuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FizzBuzz{
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("値を入力してください");
+	
+		int i = scanner.nextInt();
+		
+		if (i % 15 == 0) {
+			System.out.println("結果は…FizzBuzz");
+		}
+		else if (i % 3 == 0) {
+			System.out.println("結果は…Fizz");
+			
+		}
+		else if (i % 5 == 0) {
+			System.out.println("結果は…Buzz");
+		}
+	}
+}
